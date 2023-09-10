2df331f2f03a46a3c583961f1c9b8f4f10df1173
Prashant <prashantamoli2621@gmail.com>
2023-09-10 14:19:28 +0530

    Stripe payment intent added

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,10 @@ export default function Document() {
 			<Head>
 				<CrispChat />
 			</Head>
+
 			<body className="overflow-x-hidden transition-all duration-1000 dark:duration-1000 dark:transition-all scroll-smooth dark:scroll-smooth bg-gradient-to-br from-stone-100 via-blue-100/50 to-stone-100 dark:bg-gradient-to-br dark:from-blue-950/5 dark:via-blue-950/5 dark:to-blue-950/10 ">
 				<Main />
+
 				<NextScript />
 			</body>
 		</Html>
